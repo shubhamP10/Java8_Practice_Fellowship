456f55d6a426438d2fba6fae7e0da8633b4682dd
“Shubham” <shubhampattar2@gmail.com>
2020-06-22 21:22:41 +0530

    ADDED: Static and Default Method Implementation and Multiple Inheritance

--- src/main/java/com/bridgelabz/java8practice/Java8Practice.java
+++ src/main/java/com/bridgelabz/java8practice/Java8Practice.java
@@ -1,24 +1,29 @@
 package com.bridgelabz.java8practice;
 
 import com.bridgelabz.java8practice.models.FunctionalInterfaceImplementation;
+import com.bridgelabz.java8practice.models.StaticAndDefaultMethodInInterfaceExample;
 
 import java.util.Scanner;
 
 public class Java8Practice {
 
     public static void main(String[] args) {
-        int i = 0, choice;
+        int i = 0;
         Scanner input = new Scanner(System.in);
         while (i == 0) {
             System.out.println("JAVA 8 Features Implementation\n" +
                     "1. Functional Interface\n" +
-                    "2. Exit");
-            choice = input.nextInt();
+                    "2. Static and Default Interface\n" +
+                    "3. Exit");
+            int choice = input.nextInt();
             switch (choice) {
                 case 1:
                     FunctionalInterfaceImplementation.interfaceImplementation();
                     break;
                 case 2:
+                    StaticAndDefaultMethodInInterfaceExample.differentMethodsInInterface();
+                    break;
+                case 3:
                     i = 1;
                     break;
             }
---
+++ src/main/java/com/bridgelabz/java8practice/models/StaticAndDefaultMethodInInterfaceExample.java
@@ -0,0 +1,48 @@
+package com.bridgelabz.java8practice.models;
+
+interface SampleInterface {
+    /*Static Method*/
+    static void staticMethod() {
+        System.out.println("Static Method");
+    }
+
+    /*Abstract Method*/
+    void abstractMethod(String str);
+
+    /*Default method*/
+    default void defaultMethod() {
+        System.out.println("Default Method of SampleInterface");
+    }
+}
+
+interface SampleInterface2 {
+    default void defaultMethod() {
+        System.out.println("Default Method of SampleInterface2");
+    }
+
+    void disp(String str);
+}
+
+public class StaticAndDefaultMethodInInterfaceExample implements SampleInterface, SampleInterface2 {
+    public static void differentMethodsInInterface() {
+        StaticAndDefaultMethodInInterfaceExample obj = new StaticAndDefaultMethodInInterfaceExample();
+        obj.defaultMethod();
+        SampleInterface.staticMethod();
+        obj.abstractMethod("Calling Abstract Method");
+    }
+
+    @Override
+    public void abstractMethod(String str) {
+        System.out.println("String is: " + str);
+    }
+
+    @Override
+    public void disp(String str) {
+        System.out.println("String is: " + str);
+    }
+
+    @Override
+    public void defaultMethod() {
+        System.out.println("Implementation of Default Method");
+    }
+}
